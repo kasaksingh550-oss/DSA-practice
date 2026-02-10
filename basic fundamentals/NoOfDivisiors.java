14e11c962efda03740cab5429f6f8bd3e4c9cf28
kasak singh <kasaksingh550@gmail.com>
2026-02-10 18:41:46 +0530

    optimize progrmas

--- basic fundamentals/NoOfDivisiors.java
+++ basic fundamentals/NoOfDivisiors.java
@@ -7,9 +7,13 @@ public class NoOfDivisiors {
 
         List<Integer> list = new ArrayList<Integer>();
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i*i <= n; i++) {
             if (n % i == 0) {
                 list.add(i);
+
+                if (i != n / i) {
+                    list.add(n / i);
+                }
             }
         }
         return list;
